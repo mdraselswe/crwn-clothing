9017b8df5d2b11f7a9c02463ff520b23c2b90007
rasel <muhammad.rasel@shohoz.net>
2020-04-02 13:57:27 +0600

    setup storage - redux persist

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import "./App.css";
-
 import Header from "./components/header/header.component";
-
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
+import CheckoutPage from "./pages/checkout/checkout.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import CheckoutPage from "./pages/checkout/checkout.component";
-
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-
-  //   this.state = {
-  //     currentUser: null
-  //   };
-  // }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
