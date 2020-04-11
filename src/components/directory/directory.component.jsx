673b57a487506e09eb571ca85407756fe4f2e128
rasel <muhammad.rasel@shohoz.net>
2020-04-11 20:53:45 +0600

    updating title & favicon

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import MenuItem from "../menu-item/menu-item.component";
-
+import React from "react";
 import { connect } from "react-redux";
-import { selectDirectorySections } from "../../redux/directory/directory.selector";
-import "./directory.styles.scss";
 import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
 
 const Directory = ({ sections }) => (
   <div className="directory-menu">
@@ -15,7 +14,7 @@ const Directory = ({ sections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
+  sections: selectDirectorySections,
 });
 
 export default connect(mapStateToProps)(Directory);
