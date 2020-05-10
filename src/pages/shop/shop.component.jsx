402bf382c9c17a69a444ff348a13b0557be4f7e0
rasel <muhammad.rasel@shohoz.net>
2020-05-10 16:25:03 +0600

    change observer patern to promise patern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,13 +24,25 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionMap = convertCollectionSnapshotToMap(snapshot);
-        updateCollections(collectionMap);
-        this.setState({ loading: false });
-      }
-    );
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+    //   async (snapshot) => {
+    //     const collectionMap = convertCollectionSnapshotToMap(snapshot);
+    //     updateCollections(collectionMap);
+    //     this.setState({ loading: false });
+    //   }
+    // );
+
+    collectionRef.get().then((snapshot) => {
+      const collectionMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionMap);
+      this.setState({ loading: false });
+    });
+
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/crwn-db-1c904/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
   }
 
   render() {
