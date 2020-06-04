0d82569ebc9d0e23438b10567c52faad64e45219
rasel <muhammad.rasel@shohoz.net>
2020-06-04 15:36:24 +0600

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import "./App.css";
 import Header from "./components/header/header.component";
-import // auth,
-// createUserProfileDocument
-"./firebase/firebase.utils";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-// import { setCurrentUser } from "./redux/user/user.actions";
-import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
-// import { selectCollectionsForPreview } from "./redux/shop/shop.selector";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  // componentWillUnmount() {
-  //   this.unsubscribeFromAuth();
-  // }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,72 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
 import CollectionsOverviewContainer from "../../components/collection-overview/collection-overview.container";
-// import {
-//   convertCollectionSnapshotToMap,
-//   firestore,
-// } from "../../firebase/firebase.utils";
 import { fetchCollectionStart } from "../../redux/shop/shop.actions";
 import CollectionPageContainer from "../collection/collection.container";
 
-class ShopPage extends React.Component {
-  // state = {
-  //   loading: true,
-  // };
-
-  // unsubscribeFromSnapshot = null;
-
-  componentDidMount() {
-    const { fetchCollectionStart } = this.props;
+const ShopPage = ({ fetchCollectionStart, match }) => {
+  useEffect(() => {
     fetchCollectionStart();
+  }, [fetchCollectionStart]);
 
-    // const { updateCollections } = this.props;
-    // const collectionRef = firestore.collection("collections");
-
-    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-    //   async (snapshot) => {
-    //     const collectionMap = convertCollectionSnapshotToMap(snapshot);
-    //     updateCollections(collectionMap);
-    //     this.setState({ loading: false });
-    //   }
-    // );
-
-    // collectionRef.get().then((snapshot) => {
-    //   const collectionMap = convertCollectionSnapshotToMap(snapshot);
-    //   updateCollections(collectionMap);
-    //   this.setState({ loading: false });
-    // });
-
-    // fetch(
-    //   "https://firestore.googleapis.com/v1/projects/crwn-db-1c904/databases/(default)/documents/collections"
-    // )
-    //   .then((response) => response.json())
-    //   .then((collections) => console.log(collections));
-  }
-
-  render() {
-    const { match } = this.props;
-    // const { loading } = this.state;
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          // exact
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  // updateCollections: (collectionMap) =>
-  //   dispatch(updateCollections(collectionMap)),
   fetchCollectionStart: () => dispatch(fetchCollectionStart()),
 });
 
